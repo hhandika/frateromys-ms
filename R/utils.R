6eed32451f245f4d3ef6463a528d3dd807682567
Heru Handika <herubiolog@gmail.com>
2024-10-12 11:13:12 -0500

    Add morpho, phylo, and utils scripts.

---
+++ R/utils.R
@@ -0,0 +1,35 @@
+# Heru Handika
+# Utility function share across dataset.
+
+save_morph <- function(fnames) {
+  here("data", "morphology", fnames)
+}
+
+save_ext <- function(fname) {
+  ggsave(here("figures", fname), width = 7, height = 4)
+  system(paste("Powershell.exe Start ", here("figures", fname)))
+}
+
+save_skull <- function(fname) {
+  ggsave(here("figures", fname), width = 7, height = 4)
+  system(paste("Powershell.exe Start ", here("figures", fname)))
+}
+
+save_plot <- function(dir = "figures", fname, .width = 7, .height = 4) {
+  ggsave(here(dir, fname), width = .width, height = .height)
+
+  if (is_windows()) {
+    system(paste("Powershell.exe Start ", here(dir, fname)))
+  } else {
+    system(paste("open ", here(dir, fname)))
+  }
+}
+
+is_windows <- function() {
+  Sys.info()[["sysname"]] == "Windows"
+}
+
+save_bp <- function(fname, .width = 8, .height = 4) {
+  ggsave(here("figures", fname), width = .width, height = .height)
+  system(paste("Powershell.exe Start ", here("figures", fname)))
+}
